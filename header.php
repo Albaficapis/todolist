67fb67cfa408bd8ad31a54f61c85730da69f566a
Albaficapis <albaficapisukesu@mail.fr>
2023-01-29 16:06:05 +0100

    Mise en place du css
    Mise en place du css
    Modification de l'index pour améliorer la structure du site

--- header.php
+++ header.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./public/css/style.css">
     <script defer src="./public/js/script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+
     <title>Todo</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php include_once './header.php'; ?>
     
-
-
-<header></header>
-<div class="content"></div>
-<footer></footer>
+<div class="container">
+    <header>
+        <div class="logo">Learning ToDo</div>
+    </header>
+    <div class="content">
+        <div class="todo-container">
+            <h1>La todo</h1>
+            <div class="todo-form"></div>
+            <div class="todo-list"></div>
+        </div>
+    </div><!-- content-->
+    
+    <footer>
+    &copy; Tous droits réservés
+    </footer>
+</div> <!--container-->
 </body>
 </html>
