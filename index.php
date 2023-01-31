5b62d7978381ddff2d577f770ae4d910fb5f7f1c
Albaficapis <albaficapisukesu@mail.fr>
2023-01-31 17:49:25 +0100

    Gestion erreur Ajout de la gestion des erreurs champ vide et champ trop court Mise en place du css pour la gestion erreur

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+const ERROR_REQUIRED = "Veuillez renseigner le champ todo";
+const ERROR_TOO_SHORT = "Veuillez entrer au moins 5 caractères";
+//$error = '';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //$_POST = filter_input_array(INPUT_POST, INPUT_SANITIZE_FULL_SPECIAL_CHARS);
+    $_POST = filter_input_array(INPUT_POST, [
+            'todo' => FILTER_SANITIZE_FULL_SPECIAL_CHARS
+    ]);
+
+    $todo = $_POST['todo'] ?? '';
+
+    if(!$todo) {
+        $error = ERROR_REQUIRED;
+    } else if(strlen($todo) < 5) {
+        
+    $error = ERROR_TOO_SHORT;
+}
+}
+?>
+
+
 <?php require_once './inc/header.php'; ?>
     
 <div class="container">
@@ -8,10 +31,12 @@
         <div class="todo-container">
             <h1>La todo</h1>
             <form action="/" method="POST" class="todo-form">
-                <input type="text" name="" id="">
+                <input type="text" name="todo" id="todo">
                 <button type="submit" class="btn btn-primary">Ajouter</button>
             </form>
-     
+            <?php if($error): ?>
+            <p class="text-danger"><?= $error ?></p>
+            <?php endif; ?>
             <div class="todo-list"></div>
         </div>
     </div><!-- content-->
