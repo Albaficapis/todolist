dcc2c90a72cd373957b5b906a1e9a527962b2021
Albaficapis <albaficapisukesu@mail.fr>
2023-02-01 11:37:07 +0100

    Ajout edition et suppression todo

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
 const ERROR_REQUIRED = "Veuillez renseigner le champ todo";
 const ERROR_TOO_SHORT = "Veuillez entrer au moins 5 caractères";
-//$error = '';
+const ERROR_TOO_LONG = "Veuilez entrer au maximum 200 caractères";
+$filename = __DIR__ . "/data/todos.json";
+$error = '';
+$todos = [];
 
+if(file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $todos = json_decode($data,true) ?? [];
+}
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     //$_POST = filter_input_array(INPUT_POST, INPUT_SANITIZE_FULL_SPECIAL_CHARS);
     $_POST = filter_input_array(INPUT_POST, [
-            'todo' => FILTER_SANITIZE_FULL_SPECIAL_CHARS
-    ]);
+            'todo' => [
+                "filter" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+                "flags" => FILTER_FLAG_NO_ENCODE_QUOTES | FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_BACKTICK
+            ]
+            ]);
+            
 
     $todo = $_POST['todo'] ?? '';
 
@@ -16,7 +27,19 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else if(strlen($todo) < 5) {
         
     $error = ERROR_TOO_SHORT;
+} else if(strlen($todo) > 200) {
+    $error = ERROR_TOO_LONG;
 }
+
+    if(!$error) {
+        $todos = [...$todos, [
+            'name' => $todo,
+            'done' => false,
+            'id' => time()
+        ]];
+        file_put_contents($filename, json_encode($todos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        header('Location: /');
+    }
 }
 ?>
 
@@ -31,13 +54,25 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="todo-container">
             <h1>La todo</h1>
             <form action="/" method="POST" class="todo-form">
-                <input type="text" name="todo" id="todo">
+                <input type="text" name="todo" id="todo" value="<?= $todo ?>">
                 <button type="submit" class="btn btn-primary">Ajouter</button>
             </form>
             <?php if($error): ?>
             <p class="text-danger"><?= $error ?></p>
             <?php endif; ?>
-            <div class="todo-list"></div>
+            <ul class="todo-list">
+                <?php foreach($todos as $t): ?>
+                    <li class="todo-item <?= $t['done'] ? 'low-opacity' : ''?>">
+                        <span class="todo-name"> <?= $t['name']; ?></span>
+                        <a href="/edit-todo.php?id=<?= $t['id'] ?>">
+                            <button class="btn btn-primary btn-small"><?= $t['done'] ? 'Annuler' : 'Valider' ?></button>
+                      </a>
+                      <a href="/remove-todo.php?id=<?=$t['id'] ?>">
+                        <button class="btn btn-danger btn-small">Supprimer</button>
+                      </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </div><!-- content-->
     
